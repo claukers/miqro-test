fa8dbdeca6a55dc593feaa1134779eca705f110f
claukers <claukers@github>
2022-01-20 20:39:14 -0300

    unit testing TestHelper

---
+++ test/http.test.js
@@ -0,0 +1,61 @@
+const { it, fake, requireMock } = require("../dist/common");
+const { strictEqual } = require("assert");
+
+const options = {
+	category: "TestHelper"
+}
+
+it("happy path", async () => {
+	const badArgs = {
+		bad: "bad bad"
+	};
+	const badListener = "some bad value2";
+	const fakeUUID = "some bad uuid";
+	const fakeResponse = "some bad response";
+	const fakeListen = fake((unixSocket, cb) => {
+		strictEqual(unixSocket, `/tmp/socket.test.helper${fakeUUID}`);
+		strictEqual(fakeClose.callCount, 0);
+		cb();
+	});
+	const fakeClose = fake((cb) => {
+		strictEqual(fakeListen.callCount, 1);
+		cb();
+	});
+	const fakeServer = {
+		listen: fakeListen,
+		close: fakeClose
+	}
+	const { TestHelper } = requireMock("../dist/index.js", {
+		["@miqro/request"]: {
+			request: async (args) => {
+				strictEqual(fakeListen.callCount, 1);
+				strictEqual(fakeClose.callCount, 0);
+				strictEqual(args.bad, badArgs.bad);
+				strictEqual(args.disableThrow, true);
+				strictEqual(args.socketPath, `/tmp/socket.test.helper${fakeUUID}`);
+				return fakeResponse;
+			}
+		},
+		http: {
+			createServer: (args) => {
+				strictEqual(fakeListen.callCount, 0);
+				strictEqual(fakeClose.callCount, 0);
+				strictEqual(args, badListener);
+				return fakeServer;
+			}
+		},
+		uuid: {
+			v4: () => {
+				strictEqual(fakeListen.callCount, 0);
+				strictEqual(fakeClose.callCount, 0);
+				return fakeUUID;
+			}
+		}
+	});
+	const response = await TestHelper({
+		listener: badListener
+	}, badArgs);
+	strictEqual(response, fakeResponse);
+	strictEqual(fakeListen.callCount, 1);
+	strictEqual(fakeClose.callCount, 1);
+}, options);
