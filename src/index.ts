cb5427cad04b411be8bb4df777315359ca837548
claukers <claukers@github>
2022-01-21 17:23:06 -0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 
-export * from "./common";
-export * from "./runner";
+export { fake, requireMock, FakeCallback } from "./common";
+export { it, TestFunction } from "./runner";
 export * from "./http";
 
 export const mainPath = (): string => resolve(__dirname, "cli.js");
