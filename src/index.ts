024e5f4302efcc25d798b3c21b9f1d6c22c3104e
claukers <claukers@github>
2022-01-24 00:40:31 -0300

    remove it global

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 
 export { fake, requireMock, FakeCallback } from "./common";
-export { it, TestFunction } from "./runner";
 export * from "./http";
 
 export const mainPath = (): string => resolve(__dirname, "cli.js");
