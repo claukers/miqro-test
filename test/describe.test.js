114327101bad8f21108ee179e7b7e863f8e9acb8
claukers <claukers@github>
2022-04-22 02:07:15 -0400

    fill assertions describe tests

--- test/describe.test.js
+++ test/describe.test.js
@@ -1,57 +1,115 @@
-const { strictEqual } = require("assert");
+const {strictEqual} = require("assert");
+const {fake} = require("../dist");
 
-before(()=>{
+const beforeDescribe1 = fake(() => {
+  console.log("before describe1");
+});
+
+const afterDescribe1 = fake(() => {
+  console.log("after describe1");
+});
+
+const beforeDescribe2 = fake(() => {
   console.log("before describe2");
 });
 
-after(()=>{
+const afterDescribe2 = fake(() => {
   console.log("after describe2");
 });
 
+const beforeDescribe3 = fake(() => {
+  console.log("before describe3");
+});
+
+const afterDescribe3 = fake(() => {
+  console.log("after describe3");
+});
+
+before(beforeDescribe2);
+
+after(afterDescribe2);
+
 describe("describe1", () => {
 
-  before(()=>{
-    console.log("before describe1");
-  });
 
-  after(()=>{
-    console.log("after describe1");
-  });
 
-  it("title1", async ()=>{
+  before(beforeDescribe1);
 
+  after(afterDescribe1);
+
+  it("title1", async () => {
+
+    strictEqual(beforeDescribe1.callCount, 1);
+    strictEqual(afterDescribe1.callCount, 0);
+
+    strictEqual(beforeDescribe2.callCount, 0);
+    strictEqual(afterDescribe2.callCount, 0);
+
+    strictEqual(beforeDescribe3.callCount, 0);
+    strictEqual(afterDescribe3.callCount, 0);
   });
 
   describe("describe1 child", () => {
 
-    before(()=>{
-      console.log("before describe3");
+    before(beforeDescribe3);
+
+    after(afterDescribe3);
+
+    it("title5", async () => {
+      strictEqual(beforeDescribe1.callCount, 1);
+      strictEqual(afterDescribe1.callCount, 1);
+
+      strictEqual(beforeDescribe2.callCount, 0);
+      strictEqual(afterDescribe2.callCount, 0);
+
+      strictEqual(beforeDescribe3.callCount, 1);
+      strictEqual(afterDescribe3.callCount, 0);
     });
+    it("title6", async () => {
+      strictEqual(beforeDescribe1.callCount, 1);
+      strictEqual(afterDescribe1.callCount, 1);
 
-    after(()=>{
-      console.log("after describe3");
-    });
+      strictEqual(beforeDescribe2.callCount, 0);
+      strictEqual(afterDescribe2.callCount, 0);
 
-    it("title5", async ()=>{
-
-    });
-    it("title6", async ()=>{
-
+      strictEqual(beforeDescribe3.callCount, 2);
+      strictEqual(afterDescribe3.callCount, 1);
     });
   });
 
-  it("title2", async ()=>{
+  it("title2", async () => {
+    strictEqual(beforeDescribe1.callCount, 2);
+    strictEqual(afterDescribe1.callCount, 1);
 
+    strictEqual(beforeDescribe2.callCount, 0);
+    strictEqual(afterDescribe2.callCount, 0);
+
+    strictEqual(beforeDescribe3.callCount, 2);
+    strictEqual(afterDescribe3.callCount, 2);
   });
 
 });
 
 describe("describe2", () => {
 
-  it("title3", async ()=>{
+  it("title3", async () => {
+    strictEqual(beforeDescribe1.callCount, 2);
+    strictEqual(afterDescribe1.callCount, 2);
 
+    strictEqual(beforeDescribe2.callCount, 1);
+    strictEqual(afterDescribe2.callCount, 0);
+
+    strictEqual(beforeDescribe3.callCount, 2);
+    strictEqual(afterDescribe3.callCount, 2);
   });
-  it("title4", async ()=>{
+  it("title4", async () => {
+    strictEqual(beforeDescribe1.callCount, 2);
+    strictEqual(afterDescribe1.callCount, 2);
 
+    strictEqual(beforeDescribe2.callCount, 2);
+    strictEqual(afterDescribe2.callCount, 1);
+
+    strictEqual(beforeDescribe3.callCount, 2);
+    strictEqual(afterDescribe3.callCount, 2);
   });
 });
